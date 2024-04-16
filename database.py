458e9c24eea6186e46eb8221564d7dab6ef57958
Gideon McKinlay <gmckinlay04@gmail.com>
2024-04-16 17:28:00 +1000

    Sped up database saving considerably

--- database.py
+++ database.py
@@ -1,15 +1,17 @@
 # from replit import db
 # import pandas as pd
 
+# info = ['Name', 'Colour', 'Avatar']
 
 def saveToDB() -> None:
     # data = pd.read_csv("gwaff.csv", index_col='ID')
-    # data_cols = data.columns
     # db_cols = db.keys()
 
     # dict_form = data.to_dict()
 
     # for key in dict_form:
+    #     if key in db_cols:
+    #         continue
     #     db[key] = dict_form[key]
 
     pass
@@ -18,17 +20,11 @@ def saveToDB() -> None:
 def loadFromDB() -> None:
     # struct = {}
     # keys = list(db.keys())
-
-    # control = ['Name', 'Colour', 'Avatar']
-    # keys = [x for x in keys if x not in control]
     # keys.sort()
-    # keys = control + keys[:-1]
-
-    # struct['ID'] = db['2023-01-02 03:05:56.141261'].keys()
-
-    # for i in keys:
-    #     struct[i] = db[i].values()
+    # keys = info + keys[:-4]
+    # struct['ID'] = list(db['2023-01-02 03:05:56.141261'].keys())
+    # struct.update({i: list(db[i].values()) for i in keys})
     # df = pd.DataFrame.from_dict(struct)
     # df.to_csv('gwaff.csv', encoding='utf-8')
-    
+
     pass
