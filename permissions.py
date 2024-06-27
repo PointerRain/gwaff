5c464c066ff63b70b9229d40d266ade78bb296fe
Gideon McKinlay <gmckinlay04@gmail.com>
2024-06-27 18:26:21 +1000

    Fix permissions

--- permissions.py
+++ permissions.py
@@ -2,20 +2,22 @@ from typing import Optional, Any, Callable
 from functools import wraps
 
 import discord
+from discord import app_commands
+from discord.ext import commands
 
 ADMIN_LIST: list[int] = [344731282095472641]
 
 
-def require_admin(func: Callable):
+def require_admin(func: app_commands.AppCommand):
     @wraps(func)
     async def wrapper(
+        cog: commands.Cog,
         interaction: discord.Interaction,
         *args,
-        hidden: bool = True,
         **kwargs
     ):
         if int(interaction.user.id) in ADMIN_LIST:
-            await func(interaction, *args, **kwargs)
+            await func(cog, interaction, *args, **kwargs)
             return
         await interaction.response.send_message(":no_entry: You can't use this command", ephemeral=True)
     return wrapper
